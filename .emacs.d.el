1ed9286c88d912783217153e9cf1bb4abdbbb8e1
Emmanuel <emmanuel.mollard@epita.fr>
2022-04-04 09:57:12 +0200

    [emacs.d.el] update config

--- .emacs.d.el
+++ .emacs.d.el
@@ -1,6 +1,6 @@
-;;; personal_config.el -*- lexical-binding: t; -*-
+;;; .emacs.d.el -*- lexical-binding: t; -*-
 
-;; (load-file "~/emacs.d.el")
+;; (load-file "~/.emacs.d.el")
 
 (package-initialize)
 (setq org-agenda-files (directory-files-recursively "/home/orphee/Documents" "\\.org$"))
