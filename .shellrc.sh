0841a2de6c990645c19eb8aa7a9b1b8dd356afef
Orphee (pumpkin) <orphee.lonie@gmail.com>
2023-09-10 10:06:34 +0200

    [shellrc] change editor from vim to nvim, remove alias for MolekSyntez

--- .shellrc.sh
+++ .shellrc.sh
@@ -1,16 +1,16 @@
 set -o vi
 
-alias MolekSyntez='~/Documents/Games/MOLEK\ SYNTEZ/start.sh'
-
 screen="HDMI-1"
 alias multiScreen='xrandr --output "${screen}" --auto --right-of eDP-1'
 alias rotateScreenRight='xrandr --output "${screen}" --rotate right --auto'
 alias rotateScreenLeft='xrandr --output "${screen}" --rotate left --auto'
 alias rotateScreenNormal='xrandr --output "${screen}" --rotate normal --auto'
 
-export EDITOR=vim
+export EDITOR=nvim
+# export MANPAGER='nvim +Man!'
 
 alias keyboardGb='setxkbmap -layout gb'
 alias keyboardFR='setxkbmap -layout fr'
 
 alias egrep='egrep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox,venv}'
+alias vim=nvim
