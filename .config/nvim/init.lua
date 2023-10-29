cdb97ec8f0089bb29febcc2dae26c836934b8d72
Orphee Pumpkin <orphee.lonie@gmail.com>
2023-10-29 12:20:12 +0100

    [nvim] move config from vim file to lua file

---
+++ .config/nvim/init.lua
@@ -0,0 +1,17 @@
+vim.o.mouse=""
+vim.o.nu=true
+vim.o.rnu=true
+vim.o.hls=true
+vim.o.is=true
+vim.o.colorcolumn=81
+vim.o.list=true
+vim.o.listchars="tab:>-,trail:-,nbsp:+"
+vim.o.tabstop=4
+vim.o.shiftwidth=4
+vim.o.expandtab=true
+vim.o.winwidth=84
+
+require("lsp_keymap")
+
+-- lazy
+require("lazy_nvim")
